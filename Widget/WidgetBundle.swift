e0ff49db9a0c19c30515d256ccdc8a6d098a98e7
lszio <lszio@qq.com>
2026-05-14 19:29:02 +0800

    feat: add Widget

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by lszio on 2026/5/14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+        WidgetControl()
+        WidgetLiveActivity()
+    }
+}
